9802feb285e6cadbf8345c71260d6e0a36b29f1b
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-04-20 23:54:55 +0900

    update module.exports

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require("./dist").default;
+var lib = require("./dist");
+module.exports = lib.default;
+for (var p in lib)
+    if (!module.exports.hasOwnProperty(p)) module.exports[p] = lib[p];
+Object.defineProperty(module.exports, "__esModule", { value: true });

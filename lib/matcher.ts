e57ebabd779234ffcfd88526836bdedf00b3c3ef
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-04-20 22:34:23 +0900

    add type annotation to preset matchers

--- lib/matcher.ts
+++ lib/matcher.ts
@@ -1,23 +1,22 @@
 export type Matcher = (subject: any, condition: any) => any;
 
-export const EqualityMatcher: Matcher = (subject, condition) => {
-// export const EqualityMatcher: Matcher<Boolean> = (subject, condition) => { //TS2.3
+export const EqualityMatcher = (subject: any, condition: any) => {
   return subject === condition;
 };
 
-export const RegexpMatcher: Matcher = (subject, condition) => {
+export const RegexpMatcher = (subject: string, condition: RegExp) => {
   return subject.match(condition);
 };
 
-export const IncludingMatcher: Matcher = (subject, condition) => {
+export const IncludingMatcher = (subject: any[], condition: any) => {
   return subject.includes(condition);
 };
 
-export const IncludedMatcher: Matcher = (subject, condition) => {
+export const IncludedMatcher = (subject: any, condition: any[]) => {
   return condition.includes(subject);
 };
 
-export const FunctionMatcher: Matcher = (subject, fn) => {
+export const FunctionMatcher = (subject: any, fn: Function) => {
   return fn(subject);
 };
 
